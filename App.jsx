26fa799b2e4f5bc9319c599386aa75fc3b5958d8
Torsha-biswas <87610148+Torsha-biswas@users.noreply.github.com>
2024-04-17 14:59:03 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '/src/App.css'; // Import CSS file with styles from the src folder
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="center">Hello Kalvium</h1>
+    </div>
+  );
+}
+
+export default App;
